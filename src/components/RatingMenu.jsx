669be8aacf6a5bf22c8557fb396cd83325d9dea3
Siddharth Belwal <siddharth@Siddharths-MacBook-Pro-3.local>
2024-06-13 11:57:44 -0700

    added review ui

--- src/components/RatingMenu.jsx
+++ src/components/RatingMenu.jsx
@@ -1,23 +1,36 @@
 import '../App.css'
 import React, { useState } from "react";
+import close from "../assets/close.png";
+
 
 export default function RatingMenu({popupState, setPopupState, rating, setRating, mItem}){
     function Star({id}){
         return(
-            <div style = {(id <= rating ? {color: "orange"} : {color: "black"})} onClick = {() => {setRating(id)}} className="fa fa-star"></div>
+            <div style = {(id <= rating ? {color: "orange"} : {color: "black"})} onClick = {() => {setRating(id)}} className="fa fa-star ratingStar"></div>
         )
       }
 
     return (popupState) ? (
         <div className="popup">
             <div className="popup-inner">
-                <button className="close-popup" onClick = {() => setPopupState(false)}>close</button>
-                <h3>Ratings for {mItem.name}</h3>
-                <Star rating = {rating} setRating = {setRating}  id = {1}></Star>
-                <Star rating = {rating} setRating = {setRating}  id = {2}></Star>
-                <Star rating = {rating} setRating = {setRating}  id = {3}></Star>
-                <Star rating = {rating} setRating = {setRating}  id = {4}></Star>
-                <Star rating = {rating} setRating = {setRating}  id = {5}></Star>
+                <img className="close-popup" src={close} onClick = {() => setPopupState(false)}></img>
+                <h1 className="popupTitle">{mItem.name}</h1>
+                <h2 className="ratingTitle">Add your own review</h2>
+                <div className="rating-section">
+                    <div className="starSection">
+                        <div>
+                            <Star rating = {rating} setRating = {setRating}  id = {1}></Star>
+                            <Star rating = {rating} setRating = {setRating}  id = {2}></Star>
+                            <Star rating = {rating} setRating = {setRating}  id = {3}></Star>
+                            <Star rating = {rating} setRating = {setRating}  id = {4}></Star>
+                            <Star rating = {rating} setRating = {setRating}  id = {5}></Star>
+                        </div>
+                        <input className="ratingNameInput" type="text" placeholder="Name (optional)"></input>
+                        <button className="ratingSubmitBtn">Submit</button>
+                    </div>
+                    <textarea name="content" cols="40" rows="10" maxLength="10000" placeholder="Add a review to this menu item" className="review-input"></textarea>
+                </div>
+                <h2 className="ratingTitle">Reviews</h2>
             </div>
         </div>
     ) : "";
