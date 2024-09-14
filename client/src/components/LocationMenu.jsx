5e6e5c629c8564a7263a8e50fb50e92da563c244
evtran4 <et2mbox@gmail.com>
2024-09-13 22:38:28 -0400

    switched server

---
+++ client/src/components/LocationMenu.jsx
@@ -0,0 +1,7 @@
+export default function LocationMenu({diningHall, locationMenu}){
+    return locationMenu ? (
+        <div className = "locationMenu">
+            <img className="floorMap" src={`public/${diningHall}map.png`}></img>
+        </div>
+    ): ""
+}
